e6422063fed2c395d4ff51a02583d7ea6a8e2e73
namrahov <nurlan.amrahov@pashabank.az>
2021-09-13 22:32:20 +0400

    2nd commit

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"github.com/joho/godotenv"
+	"github.com/namrahov/gross-to-net/config"
+	log "github.com/sirupsen/logrus"
+)
+
+var opts struct {
+	Profile string `short:"p" long:"profile" default:"dev" description:"Application run profile"`
+}
 
 func main() {
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		panic(err)
+	}
+
+	initLogger()
+	initEnvVars()
+	config.LoadConfig()
+	applyLogLevel()
+
+	repo.InitDb()
+
 	fmt.Println("salam")
+}
+
+func initLogger() {
+	log.SetLevel(log.InfoLevel)
+	if opts.Profile == "default" {
+		log.SetFormatter(&log.JSONFormatter{})
+	}
+}
+
+func initEnvVars() {
+	if godotenv.Load("profiles/default.env") != nil {
+		log.Fatal("Error in loading environment variables from: profiles/default.env")
+	} else {
+		log.Info("Environment variables loaded from: profiles/default.env")
+	}
+
+	if opts.Profile != "default" {
+		profileFileName := "profiles/" + opts.Profile + ".env"
+		if godotenv.Overload(profileFileName) != nil {
+			log.Fatal("Error in loading environment variables from: ", profileFileName)
+		} else {
+			log.Info("Environment variables overloaded from: ", profileFileName)
+		}
+	}
+}
+
+func applyLogLevel() {
+	log.SetLevel(config.Props.LogLevel)
 }
---
+++ repo/db.go
@@ -0,0 +1,20 @@
+package repo
+
+import (
+	"time"
+)
+
+var Db *pg.DB
+
+func InitDb() {
+	Db = pg.Connect(&pg.Options{
+		Addr:        config.Props.DbHost + ":" + config.Props.DbPort,
+		User:        config.Props.DbUser,
+		Password:    config.Props.DbPass,
+		Database:    config.Props.DbName,
+		PoolSize:    5,
+		DialTimeout: 1 * time.Minute,
+		MaxRetries:  2,
+		MaxConnAge:  15 * time.Minute,
+	})
+}
