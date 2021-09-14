6b97de8084a7b8fc25067a099ebcbe05d9d84bd7
namrahov <nurlan.amrahov@pashabank.az>
2021-09-14 13:54:50 +0400

    6th commit

--- service/applicationService.go
+++ service/applicationService.go
@@ -2,13 +2,39 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/namrahov/ms-ecourt-go/model"
 	"github.com/namrahov/ms-ecourt-go/repo"
+	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 type IService interface {
-	GetApplications(ctx context.Context, page int64, count int64) (*model.Delivery, error)
+	GetApplications(ctx context.Context, page int64, count int64) (*model.PageableApplicationDto, error)
 }
 
 type Service struct {
 	Repo repo.IApplicationRepo
+}
+
+func (s *Service) GetApplications(ctx context.Context, page int64, count int64) (*model.PageableApplicationDto, error) {
+	logger := ctx.Value(model.ContextLogger).(*log.Entry)
+	logger.Info("GetApplications.GetDeliveryByOrderId.start")
+
+	applications, err := s.Repo.GetApplications(page, count)
+	if err != nil {
+		logger.Errorf("ActionLog.GetApplications.error: cannot get delivery details for order %d, %v", orderId, err)
+		return nil, errors.New(fmt.Sprintf("%s.can't-get-delivery", model.Exception))
+	}
+
+	pageableApplicationDto := model.PageableApplicationDto{
+		List: applications,
+	}
+
+	/*parsed, _ := time.Parse("2006-01-02 15:04:05", delivery.DeliveryDate)
+	delivery.DeliveryDate = parsed.Format(time.RFC3339)*/
+
+	logger.Info("ActionLog.GetDeliveryByOrderId.success")
+	return &pageableApplicationDto, nil
 }
