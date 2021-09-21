9f24585361771735148c22771afca5e4f2e6e38e
namrahov <nurlan.amrahov@pashabank.az>
2021-09-21 21:06:23 +0400

    commit

--- client/clientUtil.go
+++ client/clientUtil.go
@@ -2,14 +2,8 @@ package client
 
 import (
 	"bytes"
-	"context"
 	"crypto/tls"
-	"encoding/json"
-	"fmt"
-	"github.com/namrahov/ms-ecourt-go/model"
-	"io/ioutil"
 	"net/http"
-	"time"
 )
 
 func isHttpStatus2xx(httpStatusCode int) bool {
@@ -32,42 +26,4 @@ func sendRequest(httpMethod string, endpoint string, requestBody []byte) (*http.
 		return nil, err
 	}
 	return resp, nil
-}
-
-func SendRequest(ctx context.Context, request *http.Request, response interface{},
-	contentType string, method string, requestTimeout time.Duration) error {
-	request.Header = GetHeader(ctx)
-	request.Header.Del("content-type")
-	request.Header.Add("content-type", contentType)
-
-	client := http.Client{
-		Timeout: requestTimeout,
-	}
-	resp, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-
-	if !isHttpStatus2xx(resp.StatusCode) {
-		return fmt.Errorf("client %s returned http status %s", method, resp.Status)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		return err
-	}
-
-	if len(body) > 0 {
-		err = json.Unmarshal(body, &response)
-	}
-	return err
-}
-
-func GetHeader(ctx context.Context) http.Header {
-	header, ok := ctx.Value(model.ContextHeader).(http.Header)
-	if !ok {
-		return http.Header{}
-	}
-	return header
 }
