aaa20aff4e5c44929519c12f32346dcfc38f8d71
namrahov <nurlan.amrahov@pashabank.az>
2021-09-21 21:07:42 +0400

    commit

--- config/config.go
+++ config/config.go
@@ -9,16 +9,15 @@ const RootPath = "/v1/ecourt"
 const InternalRootPath = "/internal/v1/ecourt"
 
 type Args struct {
-	DbHost           string    `arg:"env:DB_CARD_DELIVERY_HOST,required"`
-	DbPort           string    `arg:"env:DB_CARD_DELIVERY_PORT,required"`
-	DbName           string    `arg:"env:DB_CARD_DELIVERY_NAME,required"`
-	DbUser           string    `arg:"env:DB_CARD_DELIVERY_USER,required"`
-	DbPass           string    `arg:"env:DB_CARD_DELIVERY_PASS,required"`
-	AdminClient      string    `arg:"env:MS_ADMIN,required"`
-	Html2PdfEndpoint string    `arg:"env:HTML_2_PDF,required"`
-	LogLevel         log.Level `arg:"env:LOG_LEVEL"`
-	Port             string    `arg:"env:PORT,required"`
-	Hostname         string    `arg:"env:HOSTNAME,required"`
+	DbHost      string    `arg:"env:DB_CARD_DELIVERY_HOST,required"`
+	DbPort      string    `arg:"env:DB_CARD_DELIVERY_PORT,required"`
+	DbName      string    `arg:"env:DB_CARD_DELIVERY_NAME,required"`
+	DbUser      string    `arg:"env:DB_CARD_DELIVERY_USER,required"`
+	DbPass      string    `arg:"env:DB_CARD_DELIVERY_PASS,required"`
+	AdminClient string    `arg:"env:MS_ADMIN,required"`
+	LogLevel    log.Level `arg:"env:LOG_LEVEL"`
+	Port        string    `arg:"env:PORT,required"`
+	Hostname    string    `arg:"env:HOSTNAME,required"`
 }
 
 var Props Args
